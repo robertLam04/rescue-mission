e1bdb1ea836ac6fab21eddf848b1a78ab8c1f4cb
robertLam04 <lamr15@mcmaster.ca>
2024-02-10 17:59:21 -0500

    Created the map and tile classes. Added method signatures for map class. (closes #12) (closes #13)

--- src/main/java/ca/mcmaster/se2aa4/island/team118/Map.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team118/Map.java
@@ -1,9 +1,47 @@
 package ca.mcmaster.se2aa4.island.team118;
+import java.util.HashMap;
+
+import ca.mcmaster.se2aa4.island.team118.Explorer.Biome;
 
 public class Map {
+    private HashMap<Integer[], Tile> TileMap;
+    private Integer[] position;
+
+    public Map() {
+        this.TileMap = new HashMap<>();
+        this.position = new Integer[]{0,0};
+    }
+
+    public Tile getTile() {
+        /* Returns the current Tile */
+        return TileMap.get(position);
+    }
+
+    public void updateFly(Direction direction) {
+        /* Update the position */
+    }
+
+    public void updateRadar(boolean isGround, Integer distance, Direction direction) {
+        /* Access the most recent radar and create the Tile */
+
+    }
+
+    public void updateScan(Biome biome, boolean isPOI, boolean isCreek) {
+        /* Need to access the most recent scan and update the current Tile */
+
+    }
+
     private class Tile {
-        
+        boolean isBorder;
+        boolean isSite;
+        boolean isCreek;
+        Biome biome;
+
+        private Tile(boolean isBorder, boolean isSite, boolean isCreek, Biome biome) {
+            this.isBorder = isBorder;
+            this.isSite = isSite;
+            this.isCreek = isCreek;
+            this.biome = biome;
+        }
     }
 }
-
-
