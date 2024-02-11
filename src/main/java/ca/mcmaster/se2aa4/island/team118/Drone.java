cd53fb76d5b43cd0339bec85bd18dd10181f0cf5
AmosEde <edeaghea@mcmaster.ca>
2024-02-10 22:49:51 -0500

    Added Methods for Drone class

--- src/main/java/ca/mcmaster/se2aa4/island/team118/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team118/Drone.java
@@ -1,14 +1,34 @@
 package ca.mcmaster.se2aa4.island.team118;
 
 public class Drone {
-    int battery;
-    Direction heading;
+    private Integer battery;
+    private Direction heading;
 
-    public Drone(int initial_battery, Direction initial_direction){
+
+    public Drone(Integer initial_battery, Direction initial_direction){
         this.battery = initial_battery;
         this.heading = initial_direction;
+
     }
-    public void updateBattery(int cost){
+    public void updateBattery(Integer cost){
+        // updates the drone battery level based on most recent action's cost
         this.battery -= cost;
     }
+
+    public void updateDirection(Direction current_heading){
+        // updates the drones current heading
+        this.heading = current_heading;
+    }
+
+
+    public Integer getBattery(){
+        // returns current battery level
+        return this.battery;
+    }
+
+    public Direction getHeading(){
+        //returns drones current direction
+        return this.heading;
+    }
+
 }
