4c75e5ec344df30957f5235db608afb1d7d9f962
AmosEde <edeaghea@mcmaster.ca>
2024-03-07 10:01:28 -0500

    initial Drone Tests

---
+++ src/test/java/ca/mcmaster/se2aa4/island/team118/DroneTest.java
@@ -0,0 +1,74 @@
+package ca.mcmaster.se2aa4.island.team118;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DroneTest {
+    private Integer battery;
+    private Direction heading;
+    private final Logger logger = LogManager.getLogger();
+    @BeforeEach
+    void setUp() {
+        this.battery = 100;
+        this.heading = Direction.E;
+    }
+
+    @Test
+    //Checks normal, extreme and erroneous cases of battery consumption for correctness
+    void updateBatteryTest() {
+        logger.info("Starting Drone updatebattery test");
+
+        Drone testDrone = new Drone(this.battery,this.heading);
+        //Normal case
+        testDrone.updateBattery(5);
+        assertEquals(0,testDrone.getBattery());
+        //Erroneous case
+        testDrone.updateBattery(-11);
+        assertEquals(95-11,testDrone.getBattery());
+        //Extreme case
+        testDrone.updateBattery(101);
+        assertEquals(0,testDrone.getBattery());
+
+
+    }
+
+    @Test
+    void updateStatusTest() {
+        logger.info("Starting Drone updateStatus method");
+        //Test that correct status is generated upon start up
+        Drone testDrone = new Drone(this.battery,this.heading);
+        assertSame(testDrone.getStatus(), Condition.Working );
+        //Confirm that status is updated appropriately
+        testDrone.updateStatus(Condition.MIA);
+        assertSame(Condition.MIA,testDrone.getStatus());
+    }
+
+    @Test
+    void distanceToStopTest() {
+        logger.info("Starting Drone distanceToStop method");
+
+        Drone testDrone = new Drone(this.battery,this.heading);
+        Double distance =  testDrone.distanceToStop();
+        assertEquals(0.0,distance);
+
+
+    }
+
+    @Test
+    void flyTest() {
+        Drone testDrone = new Drone(this.battery,this.heading);
+        testDrone.fly();
+        Position location = testDrone.getLocation();
+        assertSame(location,new Position(1,0));
+
+    }
+
+    @Test
+    void headingTest() {
+        Drone testDrone = new Drone(this.battery,this.heading);
+    }
+}
