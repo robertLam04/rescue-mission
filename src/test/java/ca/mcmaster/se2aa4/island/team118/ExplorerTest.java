22f2ecfead54d10e014aa7817db39d86cd2bf285
AmosEde <edeaghea@mcmaster.ca>
2024-03-19 23:41:02 -0400

    Decision Unit Test creation

---
+++ src/test/java/ca/mcmaster/se2aa4/island/team118/ExplorerTest.java
@@ -0,0 +1,24 @@
+package ca.mcmaster.se2aa4.island.team118;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+/*
+class ExplorerTest {
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void takeDecision() {
+    }
+
+    @Test
+    void acknowledgeResults() {
+    }
+
+    @Test
+    void deliverFinalReport() {
+    }
+}*/
---
+++ src/test/java/ca/mcmaster/se2aa4/island/team118/ManeuverTest.java
@@ -0,0 +1,53 @@
+package ca.mcmaster.se2aa4.island.team118;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManeuverTest {
+    Direction direction;
+    Direction Alternatedirection;
+    private final Logger logger = LogManager.getLogger();
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void uturnLeft() {
+    }
+
+    @Test
+    void uturnRight() {
+    }
+
+    @Test
+    void sharpTurnRight() {
+    }
+
+    @Test
+    void sharpTurnLeft() {
+    }
+
+    @Test
+    void shiftLeft() {
+    }
+
+    @Test
+    void shiftLeft2() {
+    }
+
+    @Test
+    void shiftRight() {
+    }
+
+    @Test
+    void shiftRight2() {
+    }
+
+    @Test
+    void spiral() {
+    }
+}
